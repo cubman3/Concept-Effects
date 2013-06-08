96996464e479c44fbe85987d3bcf584e9a95655e
Reed Rizzo <rrizzo@reed-mac.gateway.2wire.net>
2013-06-08 15:58:28 -0500

    Adding Scents page

--- EventScents.php
+++ EventScents.php
@@ -1,25 +1,102 @@
 
 <?php
-$page_name = '- Make up';
+$page_name = '- Custom Event Scents';
 include 'includes/header.php'; 
 ?>
 
 
 <article class="col-1">
-    <h2>Make-up</h2>
+<h2>Custom&nbsp;&nbsp;Event Scents</h2>
                  
-<p>Concept Effects works with Angela Make-up Chicago, the best Make-
-up Artists and Hair stylists in the area. With a combined 20+ years in
-their respective fields, Angela Make-up Chicago and her recognized
-professional's work with each client providing them exactly what they are
-looking for.</p>
+<p>Concept Effects is now offering Custom Scents for your event. Do you have a specific theme for your wedding, party, or event and want to add a unique touch?  Imagine walking into your room and smelling a fresh rose garden, autumn breeze, rustic wine, or chocolate.  Well now you can.  Concept Effects offers over 80 custom scents for your event and do so at a very reasonable price.</p>
+<p>Our donut pouches drop into a small fan filter, placed around the room, and sends the scent into the room. Our pods are hidden around the room so we can maintain the elegance of the space while making your event one that everyone will remember.</p>
+<p>Contact us today for more info.</p>
 
-<p>As highly trained professionals, they focus in airbrush, traditional makeup,
-bridal updo's, hair, and eyelash extensions.</p>
 
-<p>Contact Concept Effects today for more info and pricing.</p>
 
-<p><a class="link" href="Services.php">Back</a></p>
+		<div class="wrapper">
+          <ul class="list1 col-1">
+             <li>Almond</li>
+             <li>Animal Cracker</li>
+             <li>Apple Dumpling</li>
+             <li>Apple Pie</li>
+             <li>Autumn</li>
+             <li>Banana</li>
+             <li>Blueberry</li>
+             <li>Bread Pudding</li>
+             <li>Bubblegum</li>
+             <li>Buttered Corn on Cob</li>
+             <li>Cedar</li>
+             <li>Champagne</li>
+             <li>Cherry</li>
+             <li>Chocolate</li>
+             <li>Chocolate Chip Cookies</li>
+             <li>Christmas Tree</li>
+             <li>Cinnabun</li>
+             <li>Cinnamon</li>
+             <li>Cinnamon Bread</li>
+             <li>Coconut</li>
+             <li>Coffee</li>
+             <li>Cotton Candy</li>
+             <li>Cranberry</li>
+             <li>Donut</li>
+             <li>Eggnog</li>
+             <li>Evergreen</li>
+             <li>Forest</li>
+             <li>Fresh and Clean</li>
+             <li>Fresh Cut Grass</li>
+             <li>Fresh Lake Water</li>
+             <li>Fudge Brownies</li>
+             <li>Funnel Cakes</li>
+             <li>Gingerbread</li>
+             <li>Grape</li>
+             <li>Grapefruit</li>
+             <li>Grass</li>
+             <li>Green Apple</li>
+             
+          </ul>
+          <ul class="list1 col-2">
+          	<li>Hawaiian Breeze</li>
+             <li>Kiwi</li>
+             <li>Lavender</li>
+             <li>Licorice</li>
+             <li>Lemonade</li>
+             <li>Lollipop</li>
+             <li>Low Tide</li>
+             <li>Magnolia</li>
+             <li>Maple</li>
+             <li>Margarita</li>
+             <li>Mint</li>
+             <li>Merlot</li>
+             <li>Mocha</li>
+             <li>Nutmeg</li>
+             <li>Ocean</li>
+             <li>Passion Fruit</li>
+             <li>Pina Colada</li>
+             <li>Pine</li>
+             <li>Pineapple</li>
+             <li>Popcorn</li>
+             <li>Pumpkin</li>
+             <li>Pumpkin Pie</li>
+             <li>Rain</li>
+             <li>Rainforest</li>
+             <li>Roasted Cinnamon Almonds</li>
+             <li>Rose</li>
+             <li>Strawberry</li>
+             <li>Strawberry Margarita</li>
+             <li>Sugar Cookie</li>
+             <li>Tea</li>
+             <li>Vanilla</li>
+             <li>Vanilla Spice</li>
+             <li>Watermellon</li>
+             <li>Wildflower</li>
+             <li>Wine</li>
+             <li>Wine Cellar</li>
+          </ul>
+
+       </div>
+
+   <p><a class="link" href="Services.php">Back</a></p>
 
 </article>
 <article class="col-2 p2">
